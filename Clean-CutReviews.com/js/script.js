6740e1061239f5c881f5b89069e2178daf82aec3
Patrizio-programs <73086462+Patrizio-programs@users.noreply.github.com>
2021-02-15 16:15:19 -0500

    Add files via upload

---
+++ Clean-CutReviews.com/js/script.js
@@ -0,0 +1,13 @@
+function displayreview() {
+  var soul = document.getElementById("soul").value;
+
+ 
+  var soulReview = 'This is a classic. Soul is absolutely amazing, giving us an animated feel of the "Great Before" and "Great Beyond". A must watch'
+
+
+  document.getElementById("movie-review1").innerHTML = soulReview;
+
+
+}
+
+
